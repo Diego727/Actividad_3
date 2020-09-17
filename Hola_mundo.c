390225c3cde0cbbf6de58e9a4dd2ac91b7453ec7
Diego Garcia <diego.garcia2084@alumnos.udg.mx>
2020-09-17 01:08:18 -0500

    Capturamos la edad e imprimimos

--- Hola_mundo.c
+++ Hola_mundo.c
@@ -2,9 +2,14 @@
 
 int main(){
     char nombre[100];
+    unsigned int edad;
+
     printf("Hola mundo\n");
     printf("Introduzca su nombre: ");
     fgets(nombre, sizeof(nombre), stdin);
     printf("Hola %s", nombre);
+    printf("Edad: ");
+    scanf("%u", &edad);
+    printf("\nVas a cumplir %u", edad+1);
     return 0;
 }
